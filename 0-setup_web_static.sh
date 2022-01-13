72e04d05bdfe02cd6ce73e60105401c9de9dfd94
Crisgrva <crstngranada@gmail.com>
2022-01-13 01:02:52 -0500

    [ Task 0 ] Add script that sets up your web servers [ 01/13/2022/01:02 ]

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static. It must:
+# Install NGINX
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+# Creating directories
+sudo mkdir /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+
+# Giving Ownership and permissions
+sudo chown -R ubuntu: /data/
+
+# Creating fake html to test
+SIMPLE_HTML="<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>"
+echo -e $SIMPLE_HTML > /data/web_static/releases/test/index.html
+
+# Creating symbolic link
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Creating alias for location in nginx conf
+sudo chmod 777 /etc/nginx/sites-enabled/
+sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+
+# Restarting nginx
+sudo service nginx restart
